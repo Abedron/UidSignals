d9574a83085f2995ea9b011f0be5e516dcb7cbf9
Abedron <abedron@gmail.com>
2026-03-10 20:00:07 +0100

    Move logic to LogiMX class library project

--- UidSignals/MainWindow.xaml.cs
+++ UidSignals/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace UidSignals
 {
@@ -15,7 +16,7 @@ namespace UidSignals
         {
             base.OnSourceInitialized(e);
             
-            LogitechController.Initialize(this);
+            LogitechController.Initialize(PresentationSource.FromVisual(this) as HwndSource);
 
             LogitechController.ThumbWheelScrolled += OnThumbWheelScrolled;
         }
